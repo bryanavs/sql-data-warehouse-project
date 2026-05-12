a195e283f938b1c219ad2577441b8b1c2bd1db4d
bryanavs <148500133+bryanavs@users.noreply.github.com>
2026-05-11 21:25:12 -0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,21 @@
+use master;
+GO
+
+--Drop and recreate database DataWarehouse
+IF EXISTS (SELECT 1 FROM sys.databases where name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE DataWarehouse
+END;
+
+--Create Database
+CREATE DATABASE DataWarehouse;
+
+use DataWarehouse;
+
+--Create schemas
+create schema bronze;
+create schema silver;
+create schema gold;
+
 
